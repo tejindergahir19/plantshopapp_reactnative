28d5f4dfc8635845f0d4e64369e5d467aacc1e49
Tejinder Singh <tsingh@mafcarrefour.com>
2023-04-23 16:54:10 +0530

    Added cart screen

---
+++ src/components/CartItemCard.js
@@ -0,0 +1,154 @@
+import React,{useState} from "react";
+import {
+  TouchableOpacity,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Platform,
+} from "react-native";
+import COLORS from "../constant/COLORS";
+
+import Icon from "react-native-vector-icons/Ionicons";
+
+function CartItemCard(props) {
+  const { value, navigation } = props;
+
+  const [plantQuantity,setPlantQuantity] = useState(1);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Detail", value);
+      }}
+      style={styles.itemCard}
+    >
+      <View style={styles.infoSection}>
+        <View style={styles.itemImg}>
+          <Image
+            source={require("../assets/plant1.png")}
+            style={{ width: 65, height: 70 }}
+          />
+        </View>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={styles.title}>
+            {value?.title.length > 24
+              ? value.title.slice(0, 30) + "..."
+              : value.title}
+          </Text>
+
+          <View style={styles.footerItem}>
+            <Text style={styles.price}>
+              {value.price} {value.currency}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.addToWishlist}>
+          <TouchableOpacity>
+            <Icon name="close" color={COLORS.red} size={20} />
+          </TouchableOpacity>
+
+          <View style={styles.quantityMeasure}>
+            <TouchableOpacity onPress={
+              ()=>{
+                setPlantQuantity(plantQuantity - 1);
+              }
+            } style={styles.sub}>
+              <Text style={styles.subText}>-</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.quantity}>{(plantQuantity < 10) && 0}{plantQuantity}</Text>
+
+            <TouchableOpacity onPress={
+              ()=>{
+                setPlantQuantity(plantQuantity + 1);
+              }
+            } style={styles.add}>
+              <Text style={styles.addText}>+</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemCard: {
+    flex: 1,
+    backgroundColor: COLORS.grey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 10,
+    flexDirection: "column",
+  },
+  stockLeftView: {
+    flexDirection: "row-reverse",
+    justifyContent: "center",
+  },
+  stockLeft: {
+    color: COLORS.red,
+    fontSize: Platform.OS == "ios" ? 11 : 10,
+    fontWeight: "bold",
+  },
+  addToWishlist: {
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  infoSection: {
+    flexDirection: "row",
+  },
+  itemImg: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    marginRight: 6,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+    textTransform: "capitalize",
+    color: COLORS.black,
+  },
+  footerItem: {
+    marginTop: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  price: {
+    color: COLORS.primary,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  quantityMeasure:{
+    flexDirection:"row",
+  },
+  sub:{
+    backgroundColor:COLORS.primary,
+    borderRadius:50,
+    marginRight:12,
+    justifyContent:"center",
+    alignItems:"center",
+    width:26,
+    height:26
+  },
+  subText:{
+    color:COLORS.white,
+    fontSize:18,
+  },
+  add:{
+    backgroundColor:COLORS.primary,
+    borderRadius:50,
+    marginLeft:12,
+    justifyContent:"center",
+    alignItems:"center",
+    width:26,
+    height:26
+  },
+  addText:{
+    color:COLORS.white,
+    fontSize:16,
+  }
+});
+
+export default CartItemCard;
--- src/screens/Cart/CartScreen.js
+++ src/screens/Cart/CartScreen.js
@@ -1,10 +1,84 @@
-import React from "react";
-import {Text} from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  FlatList,
+} from "react-native";
 
-function CartScreen(){
-    return (
-        <Text>Cart Screen</Text>
-    )
+import Icon from "react-native-vector-icons/Ionicons";
+
+import COLORS from "../../constant/COLORS";
+import PLANTDATA from "../../constant/PLANTDATA";
+import WhishlistScreenLoader from "../../loaders/WhishlistScreenLoader";
+import CartItemCard from "../../components/CartItemCard";
+
+function CartScreen({navigation}) {
+
+  return (
+    <SafeAreaView style={[{ flex: 1 }, styles.defaults]}>
+      <View style={[styles.header, styles.iosPadding]}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <Icon name="arrow-back" size={32} color={COLORS.primary} />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>Cart</Text>
+        <View>
+          <TouchableOpacity>
+            <Icon
+              name="heart-outline"
+              color={COLORS.primaryBackgroundColor}
+              size={24}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={[{ flex: 1, paddingVertical: 20 }, styles.iosPadding]}>
+        {true ? (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={PLANTDATA}
+            renderItem={({ item }) => (
+              <CartItemCard navigation={navigation} value={item} />
+            )}
+          />
+        ) : (
+          <WhishlistScreenLoader /> // also use as <CartScreenLoader />
+        )}
+      </View>
+
+      <View style={styles.totalAmount}>
+          <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  defaults: {
+    backgroundColor: COLORS.primaryBackgroundColor,
+    padding: 20,
+  },
+  iosPadding: {
+    paddingHorizontal: Platform.OS === "ios" ? 20 : 0,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerTitle: {
+    color: COLORS.primary,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+});
 
 export default CartScreen;
