d4f2818307efdb31e44d3cf0f70f288a17a8e392
Tejinder Singh <tsingh@mafcarrefour.com>
2023-05-17 14:01:54 +0530

    profile

--- src/screens/Account/AccountOtherScreens/ProfileScreen.js
+++ src/screens/Account/AccountOtherScreens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, { useState,useRef,useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Text,
   View,
@@ -6,41 +6,92 @@ import {
   StyleSheet,
   TouchableOpacity,
   Platform,
-  BackHandler
+  BackHandler,
+  ScrollView,
+  TextInput,
+  Switch,
+  Button,
+  Alert,
+  Vibration,
 } from "react-native";
 
 import Icon from "react-native-vector-icons/Ionicons";
 
 import COLORS from "../../../constant/COLORS";
-import BottomNavigation from "../../../components/BottomNavigation";
 
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  updateProfile,
+  updatePhoneNumber,
+  updateEmail,
+} from "firebase/auth";
 
 const auth = getAuth();
 
-function ProfileScreen({navigation,route}) {
-
+function ProfileScreen({ navigation, route }) {
   const refreshAll = route?.params?.refreshAll;
 
   const userId = useRef(null);
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+
+  //form data
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
 
   const isUserLogin = async () => {
     await onAuthStateChanged(auth, (user) => {
       if (user) {
         userId.current = user.uid;
-        fetchWishlist();
+
+        setName(user?.displayName?.split("::")[0]);
+        setEmail(user?.email);
+        setPhone(user?.displayName?.split("::")[1]);
+        setAddress(user?.photoURL);
+
+        (user?.displayName?.split("::")[0] ?? setIsEnabled(true));
+        (user?.displayName?.split("::")[1] ?? setIsEnabled(true));
+        (user?.photoURL ?? setIsEnabled(true));
       } else {
         navigation.navigate("Login", { name: "Login" });
       }
     });
   };
 
+  const UpdateProfileData = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name + "::" + phone,
+      photoURL: address,
+    })
+      .then(() => {
+        updateEmail(auth.currentUser, email)
+        .then(() => {
+          Alert.alert("Profile Updated !");
+          Vibration.vibrate();
+          navigation.navigate("Account",{refreshAll:refreshAll});
+        })
+        .catch((error) => {
+          console.log("Error updating email data !", error);
+        });
+      })
+      .catch((error) => {
+        console.log("Error updating profile data !", error);
+      });
+
+   
+  };
+
   useEffect(() => {
     isUserLogin();
 
     const backHandler = BackHandler.addEventListener(
-      'hardwareBackPress',
-      ()=>{
+      "hardwareBackPress",
+      () => {
         refreshAll();
       }
     );
@@ -70,57 +121,93 @@ function ProfileScreen({navigation,route}) {
           </TouchableOpacity>
         </View>
       </View>
-
-      <View style={[{ flex: 1, paddingVertical: 20 }, styles.iosPadding]}>
-        <TouchableOpacity style={styles.accountTabs}>
-          <View style={styles.accountInnerTabs}>
-          <Icon
-              name="person"
-              color={COLORS.black}
-              size={24}
-            />
-
-            <Text style={styles.accountInnerTabsText}>Profile</Text>
-          </View>
-          <Icon
-              name="caret-forward"
-              color={COLORS.black}
-              size={24}
-            />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.accountTabs}>
-          <View style={styles.accountInnerTabs}>
-          <Icon
-              name="logo-dropbox"
-              color={COLORS.black}
-              size={24}
-            />
-
-            <Text style={styles.accountInnerTabsText}>Orders</Text>
-          </View>
-          <Icon
-              name="caret-forward"
-              color={COLORS.black}
-              size={24}
-            />
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.accountTabs}>
-          <View style={styles.accountInnerTabs}>
-          <Icon
-              name="logo-dropbox"
-              color={COLORS.black}
-              size={24}
-            />
-
-            <Text style={styles.accountInnerTabsText}>Chat with us !</Text>
-          </View>
-          <Icon
-              name="caret-forward"
-              color={COLORS.black}
-              size={24}
-            />
-        </TouchableOpacity>
+      <View style={styles.toggleContainer}>
+        <Text style={{ marginRight: 5 }}>Edit Mode</Text>
+        <Switch
+          trackColor={{ false: COLORS.red, true: COLORS.primary }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
       </View>
+
+      <ScrollView style={[{ flex: 1, paddingVertical: 20 }, styles.iosPadding]}>
+        <View style={styles.form}>
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={name ?? "Enter your name"}
+              editable={isEnabled}
+              onChangeText={(newText) => {
+                setName(newText);
+              }}
+            />
+          </View>
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={email ?? "Enter your email"}
+              editable={isEnabled}
+              onChangeText={(newText) => {
+                setEmail(newText);
+              }}
+            />
+          </View>
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Mobile</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={phone ?? "Enter your mobile"}
+              editable={isEnabled}
+              onChangeText={(newText) => {
+                setPhone(newText);
+              }}
+            />
+          </View>
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Address</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={address ?? "Enter your address"}
+              editable={isEnabled}
+              onChangeText={(newText) => {
+                setAddress(newText);
+              }}
+            />
+          </View>
+          {isEnabled && (
+            <View>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  backgroundColor: COLORS.primary,
+                  borderRadius: 6,
+                  padding: 12,
+                  marginTop: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onPress={() => {
+                  UpdateProfileData();
+                }}
+              >
+                <Text
+                  style={{
+                    color: COLORS.white,
+                    fontWeight: "bold",
+                    fontSize: 16,
+                  }}
+                >
+                  Update
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -143,25 +230,47 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 20,
   },
-  accountTabs:{
-    backgroundColor:COLORS.grey,
-    borderRadius:6,
-    justifyContent:"space-between",
-    flexDirection:"row",
-    alignItems:"center",
-    padding:15,
-    marginBottom:6
+  accountTabs: {
+    backgroundColor: COLORS.grey,
+    borderRadius: 6,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    marginBottom: 6,
   },
-  accountInnerTabs:{
-    justifyContent:"space-between",
-    flexDirection:"row",
-    alignItems:"center"
+  accountInnerTabs: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
   },
-  accountInnerTabsText:{
-    marginLeft:10,
-    fontWeight:"bold",
-    fontSize:16
-  }
+  accountInnerTabsText: {
+    marginLeft: 10,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  label: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  input: {
+    width: 343,
+    height: 42,
+    padding: 12,
+    // paddingLeft:0,
+    marginBottom: 15,
+    color: COLORS.caption,
+    borderWidth: 1,
+    borderColor: COLORS.grey,
+    borderRadius: 6,
+  },
+  toggleContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: 10,
+  },
 });
 
 export default ProfileScreen;
